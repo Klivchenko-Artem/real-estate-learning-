027a9dc9e554317d2d8749b5c7104dbf1a2244e8
Klivchenko-Artem <Artem.30393@yandex.com>
2026-04-15 21:14:41 +0700

    Fix Feedback model table name

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
+    protected $table = 'feedbacks';
+
     protected $fillable = ['name', 'phone', 'email', 'message', 'is_read'];
 
     protected $casts = [
